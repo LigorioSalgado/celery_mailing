cd947c310111932f11c31664cfd6b5c398004753
Ligorio Edwin Salgado Flores <lesf@MacBook-Pro-de-Ligorio.local>
2017-11-24 20:28:29 -0600

    first commit

---
+++ mailing/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+# Create your views here.
+
+
+
+
+class CreateUser(generics.CreateAPIView):
+
+    serializer_class = UserSerializer
+    queryset =User.objects.all()
